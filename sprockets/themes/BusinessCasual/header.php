686f365a004fcaa754e756515174290a07a45716
Ariton84 <vanver@gmail.com>
2017-12-18 07:59:15 -0800

    last mintue corrections

--- sprockets/themes/BusinessCasual/header.php
+++ sprockets/themes/BusinessCasual/header.php
@@ -42,7 +42,7 @@
               </a>
             </li>
             <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="comics.php">Comics</a>
+              <a class="nav-link text-uppercase text-expanded" href="manga.php">Manga</a>
             </li>
             <li class="nav-item px-lg-4">
               <a class="nav-link text-uppercase text-expanded" href="appointment.php">Appointment</a>
